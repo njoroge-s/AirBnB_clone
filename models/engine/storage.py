59d91f7fbc15ec894ca258df8622e2c0d691c55f
Ndindakikoma <ndindakikoma@gmail.com>
2022-10-26 23:39:10 +0300

    Commit

---
+++ models/engine/storage.py
@@ -0,0 +1,43 @@
+
+import json
+from models.base_models import BaseModel
+
+
+class FileStorage():
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+    
+    
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
+    
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
+    
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except:
+            pass
+
+
+
+          
+
+
+
