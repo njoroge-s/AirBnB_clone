54fd576f3883277b93c46f8fb85abac9b6e849cd
Ndindakikoma <ndindakikoma@gmail.com>
2022-10-30 21:52:57 +0300

    Modifications

--- README.md
+++
@@ -1,5 +0,0 @@
-Create a new object (ex: a new User or a new Place)
-Retrieve an object from a file, a database etc…
-Do operations on objects (count, compute stats, etc…)
-Update attributes of an object
-Destroy an object
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,9 @@
-from models.engine.storage import FileStorage
-storage = FileStorage
+#!/usr/bin/python3
+"""
+initialize the models package
+"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
 storage.reload()
